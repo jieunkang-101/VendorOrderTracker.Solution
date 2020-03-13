13663dd5ba1938b9252584951a2f9833478c909b
Jieun Kang <jieunkang101@gmail.com>
2020-03-13 09:59:14 -0700

    add back end logic in Models/Order.cs

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Pierre's Bakery: Vendor and Order Tracker
 
-#### C# basics Independent Project, 13 March 2020
+#### C# basic Independent Project, 13 March 2020
  
 #### By **_Jieun Kang_**
 
--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -1,9 +1,61 @@
+using System;
 using System.Collections.Generic;
 
 namespace VendorOrderTracker.Models
 {
   public class Order
   {
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+    public DateTime Date { get; set; }
+    public int Id { get; set; }
+    public static int CurrentId { get; set; } = 0;
+    public static List<Order> OrderInstances { get; } = new List<Order> {};
 
+    public Order(string title, string description, double price, DateTime date)
+    {
+      Title = title;
+      Description = description;
+      Price = price;
+      Date = date;
+      Id = CurrentId;
+      CurrentId ++;
+      OrderInstances.Add(this);
+    }
+
+    public static Order Find(int id)
+    {
+      for (int i = 0; i < OrderInstances.Count; i++)
+      {
+        if (OrderInstances[i] is Order)
+        {
+          if (OrderInstances[i].Id == id)
+          {
+            return OrderInstances[i];
+          }
+        }
+      }
+      return null;
+    }
+
+    public static void ClearAll()
+    {
+      OrderInstances.Clear();
+    }
+
+     public static void Delete(int id)
+    {
+      for (int i = 0; i < OrderInstances.Count; i++)
+      {
+        if (OrderInstances[i] is Order)
+        {
+          if (OrderInstances[i].Id == id)
+          {
+            OrderInstances.RemoveAt(i);
+          }
+        }
+      }
+    }
   }
 }  
